7c43220d204b3f98f8cb1566c37ff9eab07966eb
Shankar Veludandi <70285770+shankar-veludandi@users.noreply.github.com>
2025-05-13 23:22:22 -0400

    Add files via upload

---
+++ pacman-projects/project3-reinforcement/analysis.py
@@ -0,0 +1,68 @@
+# analysis.py
+# -----------
+# Licensing Information:  You are free to use or extend these projects for 
+# educational purposes provided that (1) you do not distribute or publish 
+# solutions, (2) you retain this notice, and (3) you provide clear 
+# attribution to UC Berkeley, including a link to 
+# http://inst.eecs.berkeley.edu/~cs188/pacman/pacman.html
+
+######################
+# ANALYSIS QUESTIONS #
+######################
+
+# Change these default values to obtain the specified policies through
+# value iteration.
+
+def question2():
+    answerDiscount = 0.9
+    answerNoise = 0
+    return answerDiscount, answerNoise
+
+def question3a():
+    answerDiscount = 0.5
+    answerNoise = 0
+    answerLivingReward = -1
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3b():
+    answerDiscount = 0.1
+    answerNoise = 0.1
+    answerLivingReward = 0
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3c():
+    answerDiscount = 1
+    answerNoise = 0
+    answerLivingReward = -1
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3d():
+    answerDiscount = 0.9
+    answerNoise = 0.2
+    answerLivingReward = 0
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3e():
+    answerDiscount = 1
+    answerNoise = 0
+    answerLivingReward = 11
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question6():
+    answerEpsilon = None
+    answerLearningRate = None
+    return 'NOT POSSIBLE'
+    return answerEpsilon, answerLearningRate
+    # If not possible, return 'NOT POSSIBLE'
+
+if __name__ == '__main__':
+    print('Answers to analysis questions:')
+    import analysis
+    for q in [q for q in dir(analysis) if q.startswith('question')]:
+        response = getattr(analysis, q)()
+        print('  Question %s:\t%s' % (q, str(response)))
